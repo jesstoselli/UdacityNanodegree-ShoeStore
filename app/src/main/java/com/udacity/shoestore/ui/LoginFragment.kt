1a76bd45d6938731f6165d9a5a7022e103f0b028
Jessyca Toselli <jfamedeiros@gmail.com>
2021-07-24 17:09:35 -0300

    Navigation on onBackPress from Welcome to Login fragment fixed

--- app/src/main/java/com/udacity/shoestore/ui/LoginFragment.kt
+++ app/src/main/java/com/udacity/shoestore/ui/LoginFragment.kt
@@ -6,16 +6,12 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.NavHostFragment.findNavController
 import com.udacity.shoestore.R
 import com.udacity.shoestore.databinding.FragmentLoginBinding
-import com.udacity.shoestore.viewmodels.LoginViewModel
 
 class LoginFragment : Fragment() {
 
-    private lateinit var viewModel: LoginViewModel
     private lateinit var binding: FragmentLoginBinding
 
     override fun onCreateView(
@@ -29,29 +25,16 @@ class LoginFragment : Fragment() {
             false
         )
 
-        viewModel = ViewModelProvider(this).get(LoginViewModel::class.java)
-        binding.loginViewModel = viewModel
-
-        viewModel.userHasLoggedIn.observe(viewLifecycleOwner, Observer { hasLoggedIn ->
-            if (hasLoggedIn) {
-                saveEmailAndPassword()
-                loginDone(hasLoggedIn)
-            }
-        })
-
-        return binding.root
-    }
-
-    private fun saveEmailAndPassword() {
-        viewModel.login = binding.emailEditText.text.toString()
-        viewModel.password = binding.passwordEditText.text.toString()
-    }
-
-    private fun loginDone(hasLoggedIn: Boolean) {
-        if (hasLoggedIn) {
+        binding.createAccountButton.setOnClickListener {
             val action = LoginFragmentDirections.actionLoginFragmentToWelcomeFragment()
             findNavController(this).navigate(action)
         }
-    }
 
+        binding.loginButton.setOnClickListener {
+            val action = LoginFragmentDirections.actionLoginFragmentToWelcomeFragment()
+            findNavController(this).navigate(action)
+        }
+
+        return binding.root
+    }
 }
